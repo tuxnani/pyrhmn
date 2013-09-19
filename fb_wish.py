6fa14b58cd13a267f5fc342f41a5da11c554aca0
Rahimanuddin Shaik <nani1only@gmail.com>
2013-09-19 13:31:38 +0530

    added my py programs

---
+++ fb_wish.py
@@ -0,0 +1,37 @@
+# Thanking everyone who wished me on my birthday
+import requests
+import json
+
+# Aman's post time
+AFTER = 1353233754
+TOKEN = '614767223'
+
+def get_posts():
+    """Returns dictionary of id, first names of people who posted on my wall
+    between start and end time"""
+    query = ("SELECT post_id, actor_id, message FROM stream WHERE "
+            "filter_key = 'others' AND source_id = me() AND "
+            "created_time > 1353233754 LIMIT 200")
+
+    payload = {'q': query, 'access_token': TOKEN}
+    r = requests.get('https://graph.facebook.com/fql', params=payload)
+    result = json.loads(r.text)
+    return result['data']
+
+def commentall(wallposts):
+    """Comments thank you on all posts"""
+    #TODO convert to batch request later
+    for wallpost in wallposts:
+
+        r = requests.get('https://graph.facebook.com/%s' %
+                wallpost['actor_id'])
+        url = 'https://graph.facebook.com/%s/comments' % wallpost['post_id']
+        user = json.loads(r.text)
+        message = 'Thanks %s :)' % user['first_name']
+        payload = {'access_token': TOKEN, 'message': message}
+        s = requests.post(url, data=payload)
+
+        print "Wall post %s done" % wallpost['post_id']
+
+if __name__ == '__main__':
+    commentall(get_posts())
---
+++ learnpy/ex5.py
@@ -0,0 +1,16 @@
+name = 'Rahimanuddin Shaik'
+age = 25 #not a lie
+height = 71 #inches
+weight = 167 #lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+print "If I add %d,%d, and %d i get %d." % (age, height, weight, age+height+weight)
